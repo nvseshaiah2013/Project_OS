5d9d3b2f0b65359967144e984743a9cca624978e
nvseshaiah2013 <26201410+nvseshaiah2013@users.noreply.github.com>
2018-03-04 00:28:09 +0530

    Added enQ and dQ

--- Queue.c
+++ Queue.c
@@ -28,15 +28,39 @@ struct Q3
 {
     struct Node *f3,*r3;
 };
+void enQ1(int a,int b,char *c)
+{
+
+}
+void deQ1()
+{
+
+}
+void enQ2(int a,int b,char *c)
+{
+
+}
+void deQ2()
+{
+
+}
+void enQ3(int a,int b,char *c)
+{
+
+}
+void deQ3()
+{
+
+}
 int main()
 {
     int n,i=0;
     //write(1,"Enter the no of Processes:\n",27);
     printf("Enter the no of Processes: \n");
-    //read(0,&n,sizeof(int));
-    scanf("%d",&n);
+    read(0,(char*)&n,sizeof(n));
+   // scanf("%d",&n);
     struct PCB arr[n];
-   // write(1,"Enter the process_name,process_burst, process_priority\n",55);
+    //write(1,"Enter the process_name,process_burst, process_priority\n",55);
    printf("Priority Ranges :\n");
     printf("Queue 1 (Round Robin): 0 - 10");
     printf("Queue 2 (Priority) : 11 - 20");
@@ -47,22 +71,22 @@ int main()
     {
         scanf("%s%d%d",arr[i].name,&arr[i].burst,&arr[i].prior);
     //    read(0,&arr,sizeof(arr));
-    if(arr[i].priot<0 || arr[i].prior> 30)
+    if(arr[i].prior<0 || arr[i].prior> 30)
     {
          printf("Not a Valid process_priority\nPlease enter process_priority again");
             scanf("%d",&arr[i].prior);
     }
     if(arr[i].prior >=0 && arr[i].prior<=10)
         {
-
+            enQ1(arr[i].burst,arr[i].prior,arr[i].name);
         }
         else if(arr[i].prior >=11 && arr[i].prior<=20)
         {
-
+            enQ2(arr[i].burst,arr[i].prior,arr[i].name);
         }
         else if(arr[i].prior >=21 && arr[i].prior<=30)
         {
-
+            enQ3(arr[i].burst,arr[i].prior,arr[i].name);
         }
         ++i;
     }
